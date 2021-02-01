d961f58155523a6e2aeddee1d0c8651814b49793
xdzurman <kamil.dzurman@gmail.com>
2021-02-01 14:04:20 +0100

    Use modified useWiewport in dashboardPage

--- app/frontend/components/common/useViewport.tsx
+++ app/frontend/components/common/useViewport.tsx
@@ -1,11 +1,14 @@
 import {ScreenSize} from '../../types'
 import {useState, useEffect} from 'preact/hooks'
 
+// we could export this and use it in other places, in that case
+// we should optimalize it with context
+// https://blog.logrocket.com/developing-responsive-layouts-with-react-hooks/
 const useViewport = (): ScreenSize => {
   const [screenSize, setScreenSize] = useState<ScreenSize>(undefined)
 
   const handleScreenResize = () => {
-    if (window.innerWidth <= 767) {
+    if (window.innerWidth < 768) {
       setScreenSize(ScreenSize.MOBILE)
     } else if (window.innerWidth <= 1024) {
       setScreenSize(ScreenSize.TABLET)
